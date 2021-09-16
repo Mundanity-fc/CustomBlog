99897c349bfffffc8d3590e54db16de0a87ab33a
Mundanity Fan <i@mundanity.moe>
2021-09-16 10:29:18 +0800

    新增文章的数据库迁移

---
+++ database/migrations/2021_09_16_022111_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('subtitle');
+            $table->string('slug')->index();
+            $table->text('body_md');
+            $table->text('body_html')->nullable();
+            $table->char('online', 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
