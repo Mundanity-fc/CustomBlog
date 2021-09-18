cfe67dbbac364568c7585328e238550597d393b9
Mundanity Fan <i@mundanity.moe>
2021-09-19 02:04:18 +0800

    修改页脚

--- resources/views/layout/_footer.blade.php
+++ resources/views/layout/_footer.blade.php
@@ -2,13 +2,9 @@
     <div class="container px-4 px-lg-5">
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
-                    <div class="small text-center text-muted">
-                        <a href="https://beian.miit.gov.cn/"><p>苏ICP备XXXXXXXX号</p></a>
-                        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
-                            <img src="/images/ba.png" style="float:left;" alt="police" />
-                            <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px;">苏公网安备 XXXXXXXXXXXXXX号</p>
-                        </a>
-                    </div>
+                <div class="small text-center text-muted"><a href="https://github.com/Mundanity-fc/CustomBlog">Check on Github</a></div>
+                <div class="small text-center text-muted"><a href="https://beian.miit.gov.cn/">苏ICP备XXXXXXXX号</a></div>
+                <div class="small text-center text-muted"><img src="/images/ba.png" alt="police" /><a href="https://beian.miit.gov.cn/">苏公网安备 XXXXXXXXXXXXXX号</a></div>
                 <div class="small text-center text-muted fst-italic">Copyright &copy; {{ env('APP_NAME') }} 2021</div>
             </div>
         </div>
