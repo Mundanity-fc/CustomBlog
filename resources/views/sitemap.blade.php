6761760cd5da3d89e370a653b475822e0611c19b
Mundanity Fan <i@mundanity.moe>
2021-10-04 13:12:56 +0800

    修改sitemap表现方式

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,20 +1,20 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-        <loc>https://mundanity.tech/</loc>
+        <loc>{{env('APP_URL')}}</loc>
         <lastmod>2021-08-01</lastmod>
     </url>
     <url>
-        <loc>https://mundanity.tech/about</loc>
+        <loc>{{env('APP_URL')}}/about</loc>
         <lastmod>2021-08-01</lastmod>
     </url>
     <url>
-        <loc>https://mundanity.tech/sitemap</loc>
+        <loc>{{env('APP_URL')}}/sitemap</loc>
         <lastmod>2021-10-02</lastmod>
     </url>
 @foreach($articles as $article)
     <url>
-         <loc>https://mundanity.tech/post/{{$article->slug}}</loc>
+         <loc>{{env('APP_URL')}}/post/{{$article->slug}}</loc>
          <lastmod>@php echo substr($article->updated_at,0,10); @endphp</lastmod>
     </url>
 @endforeach
--- resources/views/sitemap_baidu.blade.php
+++ resources/views/sitemap_baidu.blade.php
@@ -2,23 +2,23 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:mobile="http://www.baidu.com/schemas/sitemap-mobile/1/">
     <url>
-        <loc>https://mundanity.tech/</loc>
+        <loc>{{env('APP_URL')}}</loc>
         <lastmod>2021-08-01</lastmod>
         <mobile:mobile type="pc,mobile"/>
     </url>
     <url>
-        <loc>https://mundanity.tech/about</loc>
+        <loc>{{env('APP_URL')}}/about</loc>
         <lastmod>2021-08-01</lastmod>
         <mobile:mobile type="pc,mobile"/>
     </url>
     <url>
-        <loc>https://mundanity.tech/sitemap</loc>
+        <loc>{{env('APP_URL')}}/sitemap</loc>
         <lastmod>2021-10-02</lastmod>
         <mobile:mobile type="pc,mobile"/>
     </url>
 @foreach($articles as $article)
     <url>
-        <loc>https://mundanity.tech/post/{{$article->slug}}</loc>
+        <loc>{{env('APP_URL')}}/post/{{$article->slug}}</loc>
         <lastmod>@php echo substr($article->updated_at,0,10); @endphp</lastmod>
         <mobile:mobile type="pc,mobile"/>
     </url>
